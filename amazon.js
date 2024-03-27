d3e6e330dfc1917b2bebbdec02261131cabd5ca5
Payal Patil <155554292+9payal2@users.noreply.github.com>
2024-03-27 14:16:23 +0530

    Add files via upload
    added html files css files and js files and also imges

---
+++ amazon.js
@@ -0,0 +1,35 @@
+function first(){
+    document.getElementById("slideimage").src="img1.jpg";
+}
+function second(){
+    document.getElementById("slideimage").src="img2.jpg";
+}
+function third(){
+    document.getElementById("slideimage").src="img3.jpg";
+}
+function fourth(){
+    document.getElementById("slideimage").src="img4.jpg";
+}
+function fifth(){
+    document.getElementById("slideimage").src="img5.jpg";
+}
+function sixth(){
+    document.getElementById("slideimage").src="img6.jpg";
+}
+function seventh(){
+    document.getElementById("slideimage").src="img7.jpg";
+}
+function eigth(){
+    document.getElementById("slideimage").src="img8.jpg";
+}
+
+// setInterval(first,1000);
+setInterval(second,2000);
+setInterval(third,4000);
+setInterval(fourth,6000);
+setInterval(fifth,8000);
+setInterval(sixth,10000);
+setInterval(seventh,12000);
+setInterval(eigth,14000);
+
+
